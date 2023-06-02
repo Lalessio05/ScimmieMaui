fa4b5cb13a5c74bb910684b0d628e457cda236bd
Lalessio05 <catrini.alessio2@gmail.com>
2023-06-02 23:53:39 +0200

    Porca vacca ho finito.

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -6,21 +6,93 @@ public partial class MonkeysViewModel : BaseViewModel
 {
     MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
-    public MonkeysViewModel(MonkeyService monkeyService) 
+
+    IConnectivity connectivity;
+    IGeolocation geolocation;
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
+    [ObservableProperty]
+    bool isRefreshing;
+
+    [RelayCommand]
+    async Task GetClosestMonkey(Monkey monkey)
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+        try
+        {
+            //Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>()
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+            if (location is null)
+            {
+                await Shell.Current.DisplayAlert("Error!",
+                $"Could not get your location", "Ok");
+                return;
+            }
+            var first = Monkeys.OrderBy(m =>
+                location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Kilometers)
+                ).FirstOrDefault();
+            if (first is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "Awesome!");
+        }
+
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to get closest monkey: {ex.Message}", "Ok");
+        }
+    }
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null)
+            return;
+
+        await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
+            new Dictionary<string, object>
+            {
+                {"Monkey",monkey }
+            });
+    }
+
+
     [RelayCommand]
     async Task GetMonkeysAsync()
     {
-        if (IsBusy) 
+        if (IsBusy)
+        {
             return;
+        }
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet issue",
+                $"Check your internet and try again!", "Ok");
+                return;
+            }
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
-            if (Monkeys.Count != 0) 
+            if (Monkeys.Count != 0)
                 Monkeys.Clear();
             monkeys.ForEach(x => Monkeys.Add(x));
         }
@@ -30,9 +102,11 @@ public partial class MonkeysViewModel : BaseViewModel
             await Shell.Current.DisplayAlert("Error!",
                 $"Unable to get monkeys: {ex.Message}", "Ok");
         }
-        finally 
+        finally
         {
             IsBusy = false;
+            IsRefreshing = false;
+            Debug.WriteLine("Stersa");
         }
     }
 }
